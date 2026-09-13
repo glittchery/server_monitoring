90f14d7ff5147b5e67ffd850672c52467fed23bd
glittchery <glittchery>
2026-09-14 01:09:39 +0300

    disabled echo for creating tables

--- src/database/queries.py
+++ src/database/queries.py
@@ -8,8 +8,10 @@ class OrmQueries():
     @staticmethod
     async def create_tables():
         async with engine.begin() as conn:
+            engine.echo = False
             # await conn.run_sync(Base.metadata.drop_all)
             await conn.run_sync(Base.metadata.create_all)
+            engine.echo = True
 
 #       USERS
 
