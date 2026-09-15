0c04e2e9675b5fc732d9ea3e8b31175f445f9085
glittchery <glittchery>
2026-09-16 02:41:22 +0300

    JWT_COOKIE_SECURE change

--- src/auth_config.py
+++ src/auth_config.py
@@ -5,4 +5,5 @@ config.JWT_SECRET_KEY = "***"
 config.JWT_ACCESS_COOKIE_NAME = "access_token"
 config.JWT_TOKEN_LOCATION = ["cookies"]
 config.JWT_COOKIE_CSRF_PROTECT = False
+config.JWT_COOKIE_SECURE = False
 
