c85ad78dc3e69b34e170fc08a3188e3450e67604
glittchery <glittchery>
2026-09-15 22:52:01 +0300

    empty

---
+++ src/auth_config.py
@@ -0,0 +1,8 @@
+from authx import AuthXConfig
+
+config = AuthXConfig()
+config.JWT_SECRET_KEY = "***"
+config.JWT_ACCESS_COOKIE_NAME = "access_token"
+config.JWT_TOKEN_LOCATION = ["cookies"]
+config.JWT_COOKIE_CSRF_PROTECT = False
+
