54087d432960823f7dc0b6eb5ca80da099f63a89
glittchery <glittchery>
2026-09-16 17:58:01 +0300

    configuration changes

--- src/database/queries.py
+++ src/database/queries.py
@@ -11,7 +11,7 @@ class OrmQueries():
     async def create_tables():
         async with engine.begin() as conn:
             # engine.echo = False
-            await conn.run_sync(Base.metadata.drop_all)
+            # await conn.run_sync(Base.metadata.drop_all)
             await conn.run_sync(Base.metadata.create_all)
             # engine.echo = True
 
